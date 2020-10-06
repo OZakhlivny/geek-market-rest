136a007f7c810b47819e48fb7c685385e97864f1
OZakhlivny <oleg.zakhlivny@gmail.com>
2020-10-06 15:31:27 +0300

    Homework7

--- src/main/resources/static/store/store.js
+++ src/main/resources/static/store/store.js
@@ -6,18 +6,29 @@ angular.module('app').controller('storeController', function ($scope, $http) {
         $http.get(contextPath + '/api/v1/products')
             .then(function (response) {
                 $scope.Products = response.data;
+                if($scope.productFilter != null){
+                    $scope.productFilter.title = null;
+                    $scope.productFilter.price = null;
+                }
             });
     };
 
-    // $scope.applyFilter = function () {
-    //     $http({
-    //         url: contextPath + '/api/v1/books',
-    //         method: "GET",
-    //         params: {obj_title: $scope.obj.title, obj_price: $scope.obj.price}
-    //     }).then(function (response) {
-    //         ...
-    //     });
-    // }
+     $scope.applyFilter = function () {
+         $http({
+             url: contextPath + '/api/v1/products',
+             method: 'GET'
+         }).then(function (response) {
+            var filter_title = $scope.productFilter != null ? $scope.productFilter.title.toLowerCase() : '';
+            var filter_price = $scope.productFilter != null ? $scope.productFilter.price : 0;
+            if((filter_title.trim().length > 0) && (filter_price > 0)){
+                $scope.Products = [];
+                angular.forEach(response.data, function(value, key){
+                    if((value.title.toLowerCase().indexOf(filter_title) != -1) && (value.price == filter_price))
+                        $scope.Products.push(value);
+                });
+            }
+         });
+     };
 
     $scope.submitCreateNewProduct = function () {
         $http.post(contextPath + '/api/v1/products', $scope.newProduct)
